c92fb18bb96114c069e3bf9b82723637dc1fca88
niharikanallappagari <niharikareddy523@gmail.com>
2022-06-14 14:07:41 -0500

    page update

--- Login.js
+++ Login.js
@@ -1,31 +1,29 @@
 import {useState} from "react";
-import axios from "axios";
 import ProjectDetails from "./ProjectDetails";
-import VieworupdateOSP from "./vieworupdateOSP";
 
 const Login=()=>{
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [loginStatus, setLoginStatus] = useState();
-	const [responeData, setResponeData] = useState();
+	// const [email, setEmail] = useState("");
+	// const [password, setPassword] = useState("");
+	// const [loginStatus, setLoginStatus] = useState();
+	// const [responeData, setResponeData] = useState();
 	const [isViewClicked, setViewClicked] = useState(false);
 	const [projectDetails, setProjectDetails] = useState();
 
-	const handleSubmit = ()=>{
-		console.log("logged")
-		// axios.post("http://localhost:8080/validateClient",
-		// {
-		// 		email: email,
-		// 		password: password
-		// }).then(response=>{
-		// 	setLoginStatus(response.status)
-		// 	setResponeData(response.data)
-		// }).catch((error)=>{
-		// 	setLoginStatus(response.status)
-		// 	setResponeData(error) // change based on json reponse
-		// });
-			setLoginStatus(200)
-	}
+	// const handleSubmit = ()=>{
+	// 	console.log("logged")
+	// 	// axios.post("http://localhost:8080/validateClient",
+	// 	// {
+	// 	// 		email: email,
+	// 	// 		password: password
+	// 	// }).then(response=>{
+	// 	// 	setLoginStatus(response.status)
+	// 	// 	setResponeData(response.data)
+	// 	// }).catch((error)=>{
+	// 	// 	setLoginStatus(response.status)
+	// 	// 	setResponeData(error) // change based on json reponse
+	// 	// });
+	// 		setLoginStatus(200)
+	// }
 	const handleProjectDetails = (event) =>{
 		setViewClicked(true);
 		setProjectDetails({
